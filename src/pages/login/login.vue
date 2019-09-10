02d86eabed63acbdb886110b94f3a55597d8b261
Willy <willy2.009@hotmail.com>
2019-09-09 21:24:40 -0300

    css login

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <form id="loginForm">
+    <form class="form" id="loginForm">
         <div>
             <label for="inputUser">E-mail</label>
             <input v-model="email" @focus="clearError()" id="inputUser" type="text" name="username" placeholder="Username">
@@ -49,49 +49,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.login {
-  border: 1px solid black;
-  border-radius: 5px;
-  padding: 1.5rem;
-  width: 300px;
-  margin-left: auto;
-  margin-right: auto;
-  position: relative;
-  overflow: hidden;
-  .container-loading {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.3);
-    img {
-      width: 2rem;
-      height: 2rem;
-    }
+.form {
+  background-color: violet;
   }
-  form {
-    display: flex;
-    flex-flow: column;
-    *:not(:last-child) {
-      margin-bottom: 1rem;
-    }
-    input {
-      padding: 0.5rem;
-    }
-    button {
-      padding: 0.5rem;
-      background-color: lightgray;
-      border: 1px solid gray;
-      border-radius: 3px;
-      cursor: pointer;
-      &:hover {
-        background-color: lightslategray;
-      }
-    }
-  }
-}
 </style>
